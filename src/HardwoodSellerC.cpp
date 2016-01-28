01d3c35e30b90019b463ee5e21cc7ceb84b4735a
dh13f <dh13f@my.fsu.edu>
2016-01-28 09:18:50 -0500

    Final commit before submission
    Was not able to finish assignment before submission. The code compiles,
    but does not include full functionality.

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -12,42 +12,57 @@
 //============================================================================
 
 #include <iostream>
+#include <map>
+#include <array>
 #include "WoodItem.h"
+#include "WoodItem.cpp"
 using namespace std;
 
-void readInputFile(string inputFilePath);
+void readInputFile(string inputFilePath, const WoodItem * wType);
 double deliveryTime();
 
 int main() {
   string order; // Name of input file
-
-  readInputFile(order);
-
+  map<string, double> woodType;
+  WoodItem wood[6];
+  
+  wood[0] = WoodItem("Cherry", 2.5, 5.95);
+  wood[1] = WoodItem("Curly Maple", 1.5, 6.00);
+  wood[2] = WoodItem("Genuine Mahogany", 3, 9.60);
+  wood[3] = WoodItem("Wenge", 5, 22.35);
+  wood[4] = WoodItem("White Oak", 2.3, 6.70);
+  wood[5] = WoodItem("Sawdust", 1, 1.5);
+  
+  readInputFile(order, wood);
+  
   return 0;
 }
 
 /*
  * Method to read the input file
  */
-void readInputFile(string inputFilePath) {
+void readInputFile(string inputFilePath, const WoodItem * wType) {
   string name, addr, pNum;
   string hold;
+  double eta = 0.0;
   
   getline(cin, name, ';');  
   getline(cin, addr, ';');
   getline(cin, pNum, ';');
 
-  while (cin) {
-    getline(cin, hold, ';');
-    cout << hold << '\n';
-  }
+  //while() {
+  //getline(cin, hold, ':');
+    
+  //}
   
+  eta = deliveryTime();
+
   cout << "Full name: " << name << '\n';
   cout << "Delivery Address: " << addr << '\n';
   cout << "Ordered wood: " << pNum << '\n';
   cout << "Price per board foot (BF) purchased: " << '\n';
   cout << "Price: " << '\n';
-  cout << "Est. delivery time: " << '\n';
+  cout << "Est. delivery time: " << eta << '\n';
   cout << "Total price: " << '\n';  
 }
 
