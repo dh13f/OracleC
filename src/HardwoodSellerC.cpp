eec59a0fade8e511cce4b890d0a698cb96367634
dh13f <dh13f@my.fsu.edu>
2016-01-27 23:27:03 -0500

    Write main function for HardWoodSellerC.cpp
    Wrote introductory functionality prompting user to enter order text file
    in order to proceed further in program.

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -4,29 +4,53 @@
 // Version     : 1
 // Copyright   : It's free
 // Description : Main class for the Hardwood Seller
+
+/*
+  Additional code written by Dillon Hall-Rodriguez
+ */
+
 //============================================================================
 
 #include <iostream>
+#include "WoodItem.h"
 using namespace std;
 
+void readInputFile(string inputFilePath);
+double deliveryTime();
+
 int main() {
-	cout
-			<< "This is an empty main method"
-			<< endl;
-	return 0;
+  std::ifstream input;
+  std::string order;
+
+  cout << "Enter the name of the input file:";
+  cin >> order;
+  input.open(order);
+  
+  while (!input) {
+    cout << "Incorrect file name. Please try again:";
+    cin >> order;    
+    input.open(order);
+  }
+
+  readInputFile(order);
+
+  input.close();
+  
+  return 0;
 }
 
 /*
  * Method to read the input file
  */
 void readInputFile(string inputFilePath) {
-
+  ;
 }
 
 /*
  * Method to compute the deliveryTime
  */
 double deliveryTime() {
-	double deliveryETA = 0.0;
-	return deliveryETA;
+  double deliveryETA = 0.0;
+
+  return deliveryETA;
 }
